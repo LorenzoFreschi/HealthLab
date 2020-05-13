9b90b6b0a394ffb3fb9de9542653db0c2a5428e9
lfreschi <lorenzo.freschi@soprasteria.com>
2020-05-13 18:02:42 +0200

    tabella + aggiustamenti

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  purge: [],
-  theme: {
-    container: {
-      center:true,
-      margin: "1rem",
-      padding: "3rem",
-      margin:"auto"
-    }
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	theme: {
+		container: {
+			center: true,
+			margin: '1rem',
+			padding: '3rem',
+			margin: 'auto',
+		},
+		screens: {
+			sm: '500px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+	},
+	variants: {},
+	plugins: [],
+};
